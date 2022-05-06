58c4cb81f33d5c2a39d89d1a73b5ed14bf3a7c68
shada <shada@LAPTOP-IRFJU36P.myfiosgateway.com>
2022-05-05 20:24:12 -0400

    Commit3

--- src/main/java/git2/DemoTwo.java
+++ src/main/java/git2/DemoTwo.java
@@ -6,7 +6,12 @@ public class DemoTwo {
 		
 		System.out.println("Git demo 2...");
 		System.out.println("Git demo 3...");
+	}
+
+		
+		public void add(int a , int b) {
+			System.out.println(a+b);
+		}
 	
 	}
 
-}
